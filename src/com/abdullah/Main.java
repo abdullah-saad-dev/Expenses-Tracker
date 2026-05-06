83277085002682860ab76f8e4bad70e4842abd71
Abdullah Saad <abdullah.m.saad@outlook.com>
2026-05-06 18:44:25 +0300

    refact; fixed scanner leak by closing it and the end of the main method in class Main

--- src/com/abdullah/Main.java
+++ src/com/abdullah/Main.java
@@ -12,18 +12,18 @@ import java.util.Map;
 
 public class Main {
 
-
     public static void main(String[] args) {
         TransactionRepository repository = new FileTransactionRepository();
         TransactionService service = new TransactionService(repository);
         ConsoleInputReader inputReader = new ConsoleInputReader();
         Map<Integer, Command> commands = new HashMap<>();
-        ConsoleUi ui = new ConsoleUi(inputReader,commands);
-        commands.put( 1, new AddTransactionCommand(service,inputReader));
-        commands.put( 2, new ViewAllTransactionsCommand(service,inputReader));
-        commands.put( 3, new ViewTransactionsByCategoryCommand(service,inputReader));
-        commands.put( 4, new CalculateTotalTransactionsCommand(service,inputReader));
-        commands.put( 5 , new DeleteTransactionCommand(service,inputReader));
+        ConsoleUi ui = new ConsoleUi(inputReader, commands);
+        commands.put(1, new AddTransactionCommand(service, inputReader));
+        commands.put(2, new ViewAllTransactionsCommand(service, inputReader));
+        commands.put(3, new ViewTransactionsByCategoryCommand(service, inputReader));
+        commands.put(4, new CalculateTotalTransactionsCommand(service, inputReader));
+        commands.put(5, new DeleteTransactionCommand(service, inputReader));
         ui.start();
+        inputReader.close();
     }
 }
